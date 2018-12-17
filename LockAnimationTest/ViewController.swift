c6f44e1a841a8114f2a5c47cbb6eb49d99beaf1d
Ryan McLeod <ryanmcleod@gmail.com>
2018-12-17 14:30:27 +0100

    Initial Commit

---
+++ LockAnimationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LockAnimationTest
+//
+//  Created by Ryan McLeod on 12/17/18.
+//  Copyright © 2018 Grow Pixel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
